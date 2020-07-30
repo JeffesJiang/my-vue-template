3bffaaaefa8dce5d2734ff3cbdaf537d327edf81
jiangjinfu <jiangjinfu@youzidata.com>
2020-07-30 15:12:01 +0800

    feat(package.json): normalize.css
    normalize.css添加

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'normalize.css/normalize.css'; // a modern alternative to CSS resets
 
 Vue.config.productionTip = false;
 
