f28ec3f749715f6b0ac579ba39e2e6db8c3530a6
JeffesJiang <973913423@qq.com>
2020-09-18 11:22:00 +0800

    feat(HttpRequest): HttpRequest修改
    HttpRequest警告修改

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -38,8 +38,7 @@ class HttpRequest {
     // 响应拦截
     instance.interceptors.response.use(res => {
       this.destroy(url);
-      const { data, status } = res;
-      return { data, status };
+      return res;
     }, error => {
       this.destroy(url);
       let errorInfo = error.response;
